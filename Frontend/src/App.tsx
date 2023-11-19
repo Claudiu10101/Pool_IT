359752528e54358a2ab5d42cbe51b046b35ebeba
Claudiu10101 <Claudiu.alexe198@gmail.com>
2023-11-19 13:42:05 +0200

    Added React

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App(){
+  return (
+    <div>
+      <Navbar/>
+      <img src="./assets/a.webp"/>
+    </div>
+  )  
+}
+
+export default App;
